baced3c759d7f76c77cb2ce1b0a4aecb7960d948
unknown <drgs_lx@yahoo.com>
2023-12-08 15:56:15 +0200

    test

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -6,7 +6,7 @@ const Products = ({result}) => {
   return (
     <>
       {
-      result.length === 0 ? (<div className='card-container'><h2>We are deeply sorry but we didn't find anything with this criteria </h2></div>)
+      result.length === 0 ? (<div className='card-container'><h2>We are deeply sorry but we didn't find anything with this criteria. Meanwhile, you can check our other products </h2></div>)
       : (<section className='card-container'>
          {result}
       </section>)}
